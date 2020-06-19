f8c2b0a999b5b90eee0f226a75c8ae95823b341b
ABSphreak <absphreak@outlook.com>
2020-06-19 23:43:46 +0530

    ➕ Login/Register Added

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
 const connectDB = require("./config/db");
 
 // Load Config
 dotenv.config({ path: "./config/config.env" });
+
+// Passport Config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -21,11 +26,25 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+// Sessions Middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 3000;
 
