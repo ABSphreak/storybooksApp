11d3a6bc9258f970f269a99a2d0b486f2e6a6153
ABSphreak <absphreak@outlook.com>
2020-06-20 16:05:47 +0530

    ➕ Added Middlewares, helpers and more

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const connectDB = require("./config/db");
 
 // Load Config
@@ -17,13 +19,27 @@ connectDB();
 
 const app = express();
 
+// Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // Logging
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+  app.use(morgan(":id :method :url :response-time"));
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate, editIcon } = require("./helpers/hbs");
+
 // Handlebars
-app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: { formatDate, stripTags, truncate, editIcon },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set("view engine", ".hbs");
 
 // Sessions Middleware
@@ -32,6 +48,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
@@ -39,12 +56,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Set global variables
+app.use(function (req, res, next) {
+  res.locals.user = req.User || null;
+});
+
 // Static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
+app.use("/stories", require("./routes/stories"));
 
 const PORT = process.env.PORT || 3000;
 
--- routes/auth.js
+++ routes/auth.js
@@ -16,4 +16,11 @@ router.get(
   }
 );
 
+// @desc   Logout user
+// @route /auth/logout
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
 module.exports = router;
